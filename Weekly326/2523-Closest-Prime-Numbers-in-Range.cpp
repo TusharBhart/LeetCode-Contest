3f61634f5d8f4a6e363a15e665312b64b6f02659
TusharBhart <121730758+TusharBhart@users.noreply.github.com>
2023-01-01 15:31:32 +0530

    Update 2523-Closest-Prime-Numbers-in-Range.cpp

--- Weekly326/2523-Closest-Prime-Numbers-in-Range.cpp
+++ Weekly326/2523-Closest-Prime-Numbers-in-Range.cpp
@@ -1,11 +1,30 @@
 class Solution {
-public:
-    int countDigits(int num) {
-        int a = num, ans = 0;
-        while(a) {
-            if(num % (a % 10) == 0) ans++;
-            a /= 10;
+    vector<int> solve(int b) {
+        if(b <= 2) return {};
+        vector<int> v(b, 0);
+        for(int i=2; i<=sqrt(b); i++) {
+            if(!v[i]) {
+                for(int j=i*i; j<b; j += i) v[j] = 1;
+            }
         }
-        return ans;
+        return v;
+    }
+    
+public:
+    vector<int> closestPrimes(int left, int right) {
+        if(left == 1) left++;
+        vector<int> v, a, primes;
+        v = solve(right + 1);
+        for(int i=left; i<=right; i++) {
+            if(!v[i]) primes.push_back(i);
+        }
+        
+        if(primes.size() <= 1) return {-1, -1};
+        
+        int ans = 1e9;
+        for(int i=1; i<primes.size(); i++) {
+            if(primes[i] - primes[i - 1] < ans) ans = primes[i] - primes[i - 1], a = {primes[i - 1], primes[i]}; 
+        }
+        return a;
     }
 };
