b0d34a3a56aa38e86ab57e1358015fc925877810
Tushar Bhart <121730758+TusharBhart@users.noreply.github.com>
2023-01-03 12:48:49 +0530

    Update 2522-Partition-String-Into-Substrings-With-Values-at-Most-K.cpp

--- Weekly326/2522-Partition-String-Into-Substrings-With-Values-at-Most-K.cpp
+++ Weekly326/2522-Partition-String-Into-Substrings-With-Values-at-Most-K.cpp
@@ -1,30 +1,13 @@
 class Solution {
-    vector<int> solve(int b) {
-        if(b <= 2) return {};
-        vector<int> v(b, 0);
-        for(int i=2; i<=sqrt(b); i++) {
-            if(!v[i]) {
-                for(int j=i*i; j<b; j += i) v[j] = 1;
-            }
-        }
-        return v;
-    }
-    
 public:
-    vector<int> closestPrimes(int left, int right) {
-        if(left == 1) left++;
-        vector<int> v, a, primes;
-        v = solve(right + 1);
-        for(int i=left; i<=right; i++) {
-            if(!v[i]) primes.push_back(i);
+    int minimumPartition(string s, int k) {
+        int ans = 1, n = s.size();
+        string str;
+        for(int j=0; j<n; j++) {
+            if(s[j] - '0' > k) return -1;
+            str += s[j];
+            if(stol(str) > k) str = s[j], ans++;
         }
-        
-        if(primes.size() <= 1) return {-1, -1};
-        
-        int ans = 1e9;
-        for(int i=1; i<primes.size(); i++) {
-            if(primes[i] - primes[i - 1] < ans) ans = primes[i] - primes[i - 1], a = {primes[i - 1], primes[i]}; 
-        }
-        return a;
+        return ans;
     }
 };
