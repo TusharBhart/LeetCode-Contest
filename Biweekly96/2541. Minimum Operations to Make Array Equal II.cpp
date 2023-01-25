b4233c175c43b9745d109c81019a845b57a3798b
Tushar Bhart <121730758+TusharBhart@users.noreply.github.com>
2023-01-25 19:44:30 +0530

    Update 2541. Minimum Operations to Make Array Equal II.cpp

--- Biweekly96/2541. Minimum Operations to Make Array Equal II.cpp
+++ Biweekly96/2541. Minimum Operations to Make Array Equal II.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     long long minOperations(vector<int>& nums1, vector<int>& nums2, int k) {
         long long n = nums1.size(), inc = 0, dec = 0;
-        for(int i=0; i<n; i++) {
-            if(nums1[i] > nums2[i]) dec += nums1[i] - nums2[i];
-            else if(nums1[i] < nums2[i]) inc += nums2[i] - nums1[i];
-        }
-        if(inc != dec) return -1;
         if(!k) {
-            if(!inc) return 0;
-            return -1;
-        }  
-        if(inc % k == 0) return inc / k;
-        return -1;
+            for(int i=0; i<n; i++) {
+                if(nums1[i] != nums2[i]) return -1;
+            }
+            return 0;
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(nums1[i] > nums2[i]) {
+                if((nums1[i] - nums2[i]) % k) return -1;
+                dec += nums1[i] - nums2[i];
+            }
+            else if(nums1[i] < nums2[i]) {
+                if((nums2[i] - nums1[i]) % k) return -1;
+                inc += nums2[i] - nums1[i];
+            }
+        }
+        return inc != dec ? -1 : inc / k;
     }
 };
